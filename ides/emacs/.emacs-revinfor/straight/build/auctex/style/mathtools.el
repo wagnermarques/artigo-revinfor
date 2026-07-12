b731b279fc39d39b36d91eca699af0427a6f006f
wagnermarques <wagnerdocri@gmail.com>
2026-07-12 00:29:10 -0300

    auctecx C-c C-c trying to generate pdf

--- ides/emacs/.emacs-revinfor/straight/build/auctex/style/mathtools.el
+++
@@ -1,1 +0,0 @@
-/run/media/wgn/ext4/Projects-Srcs/Projects-Srcs-CDeP3/artigo-revinfor/ides/emacs/.emacs-revinfor/straight/repos/auctex/style/mathtools.el
--- ides/emacs/.emacs-revinfor/straight/build/use-package/use-package-core.el
+++
@@ -1,1 +0,0 @@
-/run/media/wgn/ext4/Projects-Srcs/Projects-Srcs-CDeP3/artigo-revinfor/ides/emacs/.emacs-revinfor/straight/repos/use-package/use-package-core.el
